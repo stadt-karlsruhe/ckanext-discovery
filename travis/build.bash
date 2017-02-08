0be544f90e82f3baff126d2391d9a1195613282c
Florian Brucker <florian.brucker@it.karlsruhe.de>
2017-02-08 11:45:04 +0100

    Configure Solr on Travis for similar_datasets

--- travis/build.bash
+++ travis/build.bash
@@ -4,6 +4,10 @@ set -e
 
 echo "This is travis-build.bash..."
 
+#
+# CKAN
+#
+
 echo "Installing the packages that CKAN requires..."
 sudo apt-get update -qq
 sudo apt-get install postgresql-$PGVERSION solr-tomcat libcommons-fileupload-java:amd64=1.2.2-1
@@ -20,34 +24,42 @@ pip install -r dev-requirements.txt --allow-all-external
 pip install coveralls
 cd -
 
-echo "Configuring Solr"
-# Solr is multicore for tests on ckan master, but it's easier to run tests on
-# Travis single-core. See https://github.com/ckan/ckan/issues/2972
-sed -i -e 's/solr_url.*/solr_url = http:\/\/127.0.0.1:8983\/solr/' ckan/test-core.ini
-sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
-# The name of the Tomcat service depends on the currently installed version
-TOMCAT_SERVICE=$(sudo service --status-all 2>&1 | awk '/tomcat/ { print $4 }')
-echo "Tomcat's service is $TOMCAT_SERVICE"
-sudo service $TOMCAT_SERVICE restart
+echo "Moving test.ini into a subdir..."
+mkdir subdir
+mv test.ini subdir
 
-echo "Creating the PostgreSQL user and database..."
-sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'pass';"
-sudo -u postgres psql -c 'CREATE DATABASE ckan_test WITH OWNER ckan_default;'
-
-echo "Initialising the database..."
-cd ckan
-paster db init -c test-core.ini
-cd -
+#
+# CKANEXT-DISCOVERY
+#
 
 echo "Installing ckanext-discovery and its requirements..."
 python setup.py develop
 pip install -r dev-requirements.txt
 
-echo "Moving test.ini into a subdir..."
-mkdir subdir
-mv test.ini subdir
+#
+# SOLR
+#
+
+echo "Configuring Solr"
+sudo cp solr/* /etc/solr/conf/
+# The name of the Tomcat service depends on the currently installed version
+TOMCAT_SERVICE=$(sudo service --status-all 2>&1 | awk '/tomcat/ { print $4 }')
+echo "Tomcat's service is $TOMCAT_SERVICE"
+sudo service $TOMCAT_SERVICE restart
+
+#
+# POSTGRESQL
+#
+
+echo "Creating the PostgreSQL user and database..."
+sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'pass';"
+sudo -u postgres psql -c 'CREATE DATABASE ckan_test WITH OWNER ckan_default;'
+
+echo "Initialising CKAN database..."
+paster --plugin=ckan db init -c subdir/test.ini
 
 echo "Initializing ckanext-discovery database tables..."
 paster --plugin=ckanext-discovery search_suggestions init -c subdir/test.ini
 
 echo "travis-build.bash is done."
+
