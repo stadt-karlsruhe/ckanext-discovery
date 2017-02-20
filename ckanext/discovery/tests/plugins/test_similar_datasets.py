87f5efd198c3a0774b352c5fb5462a0e787a618d
Florian Brucker <florian.brucker@it.karlsruhe.de>
2017-02-20 14:01:51 +0100

    Tests for similar_datasets UI

--- ckanext/discovery/tests/plugins/test_similar_datasets.py
+++ ckanext/discovery/tests/plugins/test_similar_datasets.py
@@ -8,21 +8,20 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 from nose.tools import eq_, assert_not_in, ok_
+from routes import url_for
+from bs4 import BeautifulSoup
 
 import ckan.tests.helpers as helpers
 import ckan.tests.factories as factories
 
 from ...plugins.similar_datasets import get_similar_datasets
-from .. import changed_config
+from .. import changed_config, purge_datasets
 
 
-class TestGetSimilarDatasets(helpers.FunctionalTestBase):
-    '''
-    Test ``get_similar_datasets``.
-    '''
+class SimilarDatasetsTestBase(helpers.FunctionalTestBase):
     def setup(self):
         # The super class method clears the DB and the search index
-        super(TestGetSimilarDatasets, self).setup()
+        super(SimilarDatasetsTestBase, self).setup()
         self.datasets = [
             factories.Dataset(title='cat dog wolf'),
             factories.Dataset(title='cat dog fox'),
@@ -36,6 +35,11 @@ class TestGetSimilarDatasets(helpers.FunctionalTestBase):
         for dataset in self.datasets:
             print('  {} "{}"'.format(dataset['id'], dataset['title']))
 
+
+class TestGetSimilarDatasets(SimilarDatasetsTestBase):
+    '''
+    Test ``get_similar_datasets``.
+    '''
     def assert_not_similar(self, datasets):
         '''
         Assert that datasets are not similar to the first test dataset.
@@ -102,4 +106,58 @@ class TestGetSimilarDatasets(helpers.FunctionalTestBase):
         private = factories.Dataset(title=self.datasets[0]['title'],
                                     owner_org=org['id'], private=True)
         self.assert_not_similar([private])
+
+
+class TestUI(SimilarDatasetsTestBase):
+    '''
+    Test web UI.
+    '''
+    def make_soup(self, **kwargs):
+        app = self._get_test_app()
+        url = url_for(**kwargs)
+        response = app.get(url)
+        body = response.body.decode('utf-8')
+        return BeautifulSoup(body)
+
+    def test_package_read_template(self):
+        '''
+        Similar datasets are listed on the dataset view.
+        '''
+        soup = self.make_soup(controller='package', action='read',
+                              id=self.datasets[0]['id'])
+        section = soup.find('section', class_='similar-datasets')
+        ok_(section is not None)
+
+    def test_no_similar_datasets(self):
+        '''
+        If there are no similar datasets then no list is shown at all.
+        '''
+        purge_datasets()
+        dataset = factories.Dataset()
+        soup = self.make_soup(controller='package', action='read',
+                              id=dataset['id'])
+        section = soup.find('section', class_='similar-datasets')
+        ok_(section is None)
+
+    def test_disabled_suggestions(self):
+        '''
+        The maximum number of listed similar datasets can be configured.
+        '''
+        n = len(self.datasets)
+
+        def assert_number_of_items(expected):
+            soup = self.make_soup(controller='package', action='read',
+                                  id=self.datasets[0]['id'])
+            section = soup.find('section', class_='similar-datasets')
+            if expected == 0:
+                ok_(section is None)
+            else:
+                items = section.ul.find_all('li')
+                eq_(len(items), min(n - 1, expected))
+
+        assert_number_of_items(5)  # Default value
+        key = 'ckanext.discovery.similar_datasets.max_num'
+        for max_num in [0, 1, 2, n - 1, n, n + 1]:
+            with changed_config(key, max_num):
+                assert_number_of_items(max_num)
 
